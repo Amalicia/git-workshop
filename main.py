6c21dc344217d182b7d755a2ed60f0f2c179b91a
Nick <19692202@students.lincoln.ac.uk>
2019-10-15 19:52:04 +0100

    marmalade

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 # your contributions here
+def uselessFunction():
+    name = input("What is your name? ")
+    print("Nice to meet you {0}".format(name))
+    for i in range(10):
+        print("Marmalade")
+
+uselessFunction()
