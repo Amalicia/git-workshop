93abcfb661ee8d66799f1e6ece8ece7bebb1949f
nenocen <nicholas.suckling@gmail.com>
2019-10-15 20:06:46 +0100

    marmalade

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # your contributions here
 def uselessFunction():
     name = input("What is your name? ")
-    print("Hello, nice to meet you {0}".format(name))
+    print("Nice to meet you {0}".format(name))
     for i in range(10):
         print("Marmalade")
 
