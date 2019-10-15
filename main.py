25a56c80d950f1d038f9356e599eeccb6cf7bbde
JJMaslen <44002981+JJMaslen@users.noreply.github.com>
2019-10-15 19:22:38 +0100

    Add files via upload
    Latest Changes ;)

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # your contributions here
+
+# Jordan's Code Start
+
+bean = "beans"
+
+if bean == "beans":
+    print(bean)
+    print(bean)
+    print(bean)
+    print(bean)
+    print(bean)
+    print(bean)
+
+# Jordan's Code End
